146011def373c2b811e5ebdf52d1e5712479d2b0
v0agent <it+v0agent@vercel.com>
2026-06-25 14:52:50 +0000

    feat: update iframe dimensions and margins in Home component
    Co-authored-by: rajeshrk9 <293481995+rajeshrk9@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -278,10 +278,10 @@ export default function Home() {
               <iframe 
                 src="https://docs.google.com/forms/d/e/1FAIpQLSf9FmhjmCeClqH6b-A-tZiYPKilMoEOtw1naugvWhAqMzWjtA/viewform?embedded=true" 
                 width="100%" 
-                height="900" 
+                height={900}
                 frameBorder="0" 
-                marginHeight="0" 
-                marginWidth="0"
+                marginHeight={0}
+                marginWidth={0}
                 className="w-full"
               >
                 Loading…
